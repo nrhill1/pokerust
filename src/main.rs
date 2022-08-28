3cafdcd6bbbb0310ea2f5bf0cafb733c87b0d19d
Nic Hill <nrhill1@gmail.com>
2022-08-28 11:02:12 -0700

    adding impl for Move and Pokemon

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,17 @@ pub struct Move {
     pub move_type: String,
 }
 
+impl Move {
+    pub fn new(name: String, damage: u32, accuracy: u32, move_type: String) -> Self {
+        Self {
+            name,
+            damage,
+            accuracy,
+            move_type,
+        }
+    }
+}
+
 pub struct Pokemon {
     pub name: String,
     pub level: u8,
@@ -18,6 +29,33 @@ pub struct Pokemon {
     pub moves: Vec<Move>,
 }
 
+impl Pokemon {
+    pub fn new(
+        name: String,
+        level: u8,
+        hp: u8,
+        attack: u8,
+        defense: u8,
+        speed: u8,
+        special_attack: u8,
+        special_defense: u8,
+        types: Vec<String>,
+        moves: Vec<Move>,
+    ) -> Self {
+        Self {
+            name,
+            level,
+            hp,
+            attack,
+            defense,
+            speed,
+            special_attack,
+            special_defense,
+            types,
+            moves,
+        }
+    }
+}
 
 fn main() {
     println!("Hello, world!");
